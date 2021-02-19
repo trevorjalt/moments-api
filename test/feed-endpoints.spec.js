914c11b0cc308c9c3298f5d20ea923a1d7db3424
Trevor J Alt <trevor@trevorjalt.com>
2021-02-19 14:51:51 -0500

    prepared for deploy

--- test/feed-endpoints.spec.js
+++ test/feed-endpoints.spec.js
@@ -18,7 +18,7 @@ describe('Feed Endpoints', function() {
     before('make knex instance', () => {
         db = knex({
             client: 'pg',
-            connection: process.env.TEST_DB_URL,
+            connection: process.env.TEST_DATABASE_URL,
         })
         app.set('db', db)
     })
