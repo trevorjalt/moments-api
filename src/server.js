b5ccaf128e94a3857341af91b780da4c20e1c003
Trevor J Alt <trevor@trevorjalt.com>
2021-02-19 15:17:36 -0500

    deploy bug

--- src/server.js
+++ src/server.js
@@ -4,6 +4,11 @@ const knex = require('knex')
 const app = require('./app');
 const { PORT, DATABASE_URL } = require('./config');
 
+if (process.env.NODE_ENV === "production") {
+    const pg = require('pg');
+    pg.defaults.ssl = { rejectUnauthorized: false }
+}
+
 const db = knex({
     client: 'pg',
     connection: DATABASE_URL,
