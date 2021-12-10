a3fece7f909c45733ee5e097576872d6f5c4322b
Trevor J Alt <trevor@trevorjalt.com>
2021-12-09 19:36:24 -0500

    Update

--- test/auth-endpoints.spec.js
+++ test/auth-endpoints.spec.js
@@ -13,7 +13,7 @@ describe('Auth Endpoints', function () {
     before('make knex instance', () => {
         db = knex({
             client: 'pg',
-            connection: process.env.TEST_DATABASE_URL,
+            connection: process.env.TEST_DB_URL,
         })
         app.set('db', db)
     })
--- test/protected-endpoints.spec.js
+++ test/protected-endpoints.spec.js
@@ -16,7 +16,7 @@ describe('Protected endpoints', function() {
     before('make knex instance', () => {
         db = knex({
         client: 'pg',
-        connection: process.env.TEST_DATABASE_URL,
+        connection: process.env.TEST_DB_URL,
         })
         app.set('db', db)
     })
